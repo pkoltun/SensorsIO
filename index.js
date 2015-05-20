b29f8d9cf87bea3ac1b02feb0222864e6e3ef789
Paweł Kołtuniuk <pkoltuniuk@gmail.com>
2015-05-20 21:03:02 +0200

    Export WebSocketServer

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ module.exports.Sensor = require('./lib/Sensor.js')
 module.exports.SensorsServer = require('./lib/SensorsServer.js')
 module.exports.SensorUtils = require('./lib/SensorUtils.js')
 module.exports.SensorsServerTest = require('./lib/SensorsServerTest.js')
+module.exports.WebSocketServer = require('./lib/WebSocketServer.js');
